6e6d7cce5af9320be281240173ff168f01be64a4
u-jaswanth2006 <jassullamgunta06@gmail.com>
2025-11-19 01:43:02 +0530

    first commit of java project

---
+++ src/main/java/com/example/demo_java_app/App.java
@@ -0,0 +1,13 @@
+package com.example.demo_java_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from maven java project" );
+    }
+}
